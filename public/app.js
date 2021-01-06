817c20c762c3934ac38ff5ab9bc2da5f18c8ac9b
Miko Nuñez <nunyezmiko@gmail.com>
2021-01-06 23:16:28 +0800

    added remaining gallery card

--- public/app.js
+++ public/app.js
@@ -1,11 +1,24 @@
+var gallery1Btn = document.querySelector('#gallery1Btn');
+var gallery1 = document.querySelector('#gallery1');
+
 var gallery2Btn = document.querySelector('#gallery2Btn');
 var gallery2 = document.querySelector('#gallery2');
-var gallery2CloseBtn = document.querySelector('#gallery2CloseBtn');
 
+var gallery1Close = document.querySelector('#gallery1Close');
+var gallery2Close = document.querySelector('#gallery2Close');
+
+// modal activation buttons
+gallery1Btn.addEventListener('click', function (){
+    gallery1.classList.remove('invisible');
+});
 gallery2Btn.addEventListener('click', function (){
     gallery2.classList.remove('invisible');
 });
 
-gallery2CloseBtn.addEventListener('click', function(){
+// close buttons
+gallery1Close.addEventListener('click', function(){
+    gallery1.classList.add('invisible');
+});
+gallery2Close.addEventListener('click', function (){
     gallery2.classList.add('invisible');
 });
